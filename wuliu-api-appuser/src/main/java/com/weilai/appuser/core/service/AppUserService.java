8599ef57078bb01810cf03781be275d6ed571d36
liyongzhen <liyongzhen@zhidianlife.com>
2018-03-08 12:58:48 +0800

    生成resources

--- wuliu-api-appuser/src/main/java/com/weilai/appuser/core/service/AppUserService.java
+++ wuliu-api-appuser/src/main/java/com/weilai/appuser/core/service/AppUserService.java
@@ -1,8 +1,8 @@
 package com.weilai.appuser.core.service;
 
-import com.weilai.common.service.BaseService;
 import com.weilai.appuser.core.model.AppUser;
+import com.weilai.common.service.BaseService;
 
 public interface AppUserService extends BaseService<AppUser> {
-
+	public void updateAppUserName();
 }
--- wuliu-service-appuser/src/main/java/com/weilai/appuser/core/service/impl/AppUserServiceImpl.java
+++ wuliu-service-appuser/src/main/java/com/weilai/appuser/core/service/impl/AppUserServiceImpl.java
@@ -3,21 +3,25 @@ package com.weilai.appuser.core.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.weilai.common.service.impl.BaseServiceImpl;
-import com.weilai.common.dao.BaseDao;
 import com.weilai.appuser.core.dao.AppUserDao;
 import com.weilai.appuser.core.model.AppUser;
 import com.weilai.appuser.core.service.AppUserService;
-
+import com.weilai.common.dao.BaseDao;
+import com.weilai.common.service.impl.BaseServiceImpl;
 
 @Service("appUserService")
-public class AppUserServiceImpl extends BaseServiceImpl<AppUser> implements AppUserService{
+public class AppUserServiceImpl extends BaseServiceImpl<AppUser> implements AppUserService {
 
 	@Autowired
 	private AppUserDao appUserDaoImpl;
-	
+
 	@Override
 	public BaseDao<AppUser> getBaseDao() {
 		return appUserDaoImpl;
+	}
+
+	@Override
+	public void updateAppUserName() {
+
 	}
 }
