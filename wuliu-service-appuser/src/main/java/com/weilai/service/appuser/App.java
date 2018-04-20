c8ce3b3985bffe7fc34c4eb707dba0396c499d0a
liyongzhen <liyongzhen@zhidianlife.com>
2018-04-20 09:59:14 +0800

    213

--- wuliu-service-appuser/src/main/java/com/weilai/service/appuser/App.java
+++ wuliu-service-appuser/src/main/java/com/weilai/service/appuser/App.java
@@ -1,13 +1,35 @@
 package com.weilai.service.appuser;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		// System.out.println("Hello World!");
+		// List<String> orders = new ArrayList<>();
+		// orders.add("a");
+		// orders.add("b");
+		// List<String> oneBatchNumList = new ArrayList<String>();
+		// Iterator<String> it = orders.iterator();
+		// while (it.hasNext()) {
+		// if (it.next().equals("a")) {
+		// it.remove();
+		// }
+		// }
+		// for (String cc : orders) {
+		// System.out.println(cc);
+		// }
+		System.out.println(TmsType.CUSTOMER.name().equals("CUSTOMER"));
+
+	}
+
+	public enum TmsType {
+		CUSTOMER("C"), STORAGE("W"), STORE("S"), ORDER("G");
+		private String value;
+
+		TmsType(String value) {
+			this.value = value;
+		}
+
+		public String getCode() {
+			return this.value;
+		}
+	}
 }
